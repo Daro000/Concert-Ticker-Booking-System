511c2fd7d86b45fd44255b489d2ea5c5ba196408
daro <dfghjk@email.com>
2024-12-10 08:14:50 +0100

    Utworzenie klasy Concert z wlasciwosciami name, date, location, availabeSeats

---
+++ Concert Ticket Booking System/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Concert
+{
+    string name {get; set;}
+    string date {get; set;}
+    string location {get; set;}
+    string availableSeats {get; set;}
+
+    public Concert(string name, string date, string location, string availableSeats)
+    {
+        this.name = name;
+        this.date = date;
+        this.location = location;
+        this.availableSeats = availableSeats;
+    }
+}
+        
